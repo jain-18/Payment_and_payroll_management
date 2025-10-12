2596a69458541a99806ec2193da5927f1d94e6d5
unknown <shubham.jain>
2025-10-12 11:40:27 +0530

    completed admin controller

--- src/main/java/com/payment/controller/AdminController.java
+++ src/main/java/com/payment/controller/AdminController.java
@@ -8,9 +8,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.payment.dto.PendingVendorRes;
+import com.payment.dto.VendorRequestRes;
 import com.payment.dto.RequestReasonDto;
 import com.payment.dto.RequestResp;
+import com.payment.dto.SalaryRequestRes;
 import com.payment.service.AdminService;
 import com.payment.service.VendorService;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,41 +28,98 @@ public class AdminController {
     @Autowired
     AdminService adminService;
 
-    @GetMapping("/pending")
-    public ResponseEntity<Page<PendingVendorRes>> getAllPendingVendor(
+    @GetMapping("/pendingVendor")
+    public ResponseEntity<Page<VendorRequestRes>> getAllPendingVendor(
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size,
             @RequestParam(defaultValue = "requestDate") String sortBy) {
         PageRequest pageable = PageRequest.of(page, size, Sort.by(sortBy).ascending());
-        Page<PendingVendorRes> response = adminService.getAllPendingVendorRequest(pageable);
+        Page<VendorRequestRes> response = adminService.getALLVendorRequestByStatus(pageable, "PENDING",
+                "VENDORPAYMENT");
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/approvedVendor")
+    public ResponseEntity<Page<VendorRequestRes>> getAllApprovedVendor(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "requestDate") String sortBy) {
+        PageRequest pageable = PageRequest.of(page, size, Sort.by(sortBy).ascending());
+        Page<VendorRequestRes> response = adminService.getALLVendorRequestByStatus(pageable, "APPROVED",
+                "VENDORPAYMENT");
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/rejectedVendor")
+    public ResponseEntity<Page<VendorRequestRes>> getAllRejectedVendor(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "requestDate") String sortBy) {
+        PageRequest pageable = PageRequest.of(page, size, Sort.by(sortBy).ascending());
+        Page<VendorRequestRes> response = adminService.getALLVendorRequestByStatus(pageable, "REJECTED",
+                "VENDORPAYMENT");
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/pendingSalary")
+    public ResponseEntity<Page<SalaryRequestRes>> getAllPendindSalary(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "requestDate") String sortBy) {
+        PageRequest pageable = PageRequest.of(page, size, Sort.by(sortBy).ascending());
+        Page<SalaryRequestRes> response = adminService.getALLSalaryRequestByStatus(pageable, "PENDING",
+                "SALARYPAYMENT");
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/approvedSalary")
+    public ResponseEntity<Page<SalaryRequestRes>> getAllApprovedSalary(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "requestDate") String sortBy) {
+        PageRequest pageable = PageRequest.of(page, size, Sort.by(sortBy).ascending());
+        Page<SalaryRequestRes> response = adminService.getALLSalaryRequestByStatus(pageable, "APPROVED",
+                "SALARYPAYMENT");
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/rejectedSalary")
+    public ResponseEntity<Page<SalaryRequestRes>> getAllRejectedSalary(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "requestDate") String sortBy) {
+        PageRequest pageable = PageRequest.of(page, size, Sort.by(sortBy).ascending());
+        Page<SalaryRequestRes> response = adminService.getALLSalaryRequestByStatus(pageable, "REJECTED",
+                "SALARYPAYMENT");
         return ResponseEntity.ok(response);
     }
 
     @GetMapping("/singleRequest")
-    public ResponseEntity<RequestResp> getAllPendingVendor(@RequestParam Long requestId){
+    public ResponseEntity<RequestResp> getAllPendingVendor(@RequestParam Long requestId) {
         RequestResp requestResp = adminService.getSingleRequest(requestId);
         return ResponseEntity.ok(requestResp);
     }
+
     @PostMapping("/vendorRequestApproved")
-    public ResponseEntity<RequestResp> requestApproved(@RequestParam Long requestId){
+    public ResponseEntity<RequestResp> requestApproved(@RequestParam Long requestId) {
         RequestResp requestResp = adminService.vendorRequestApproved(requestId);
         return ResponseEntity.ok(requestResp);
     }
 
     @PostMapping("/vendorRequestRejected")
-    public ResponseEntity<RequestResp> requestApproved(@RequestBody RequestReasonDto dto){
+    public ResponseEntity<RequestResp> requestApproved(@RequestBody RequestReasonDto dto) {
         RequestResp requestResp = adminService.vendorRequestReject(dto);
         return ResponseEntity.ok(requestResp);
     }
 
     @PostMapping("/salaryRequestApproved")
-    public ResponseEntity<RequestResp> salaryRequestApproved(@RequestParam Long requestId){
+    public ResponseEntity<RequestResp> salaryRequestApproved(@RequestParam Long requestId) {
         RequestResp requestResp = adminService.approveSalaryRequest(requestId);
         return ResponseEntity.ok(requestResp);
     }
 
     @PostMapping("/salaryRequestReject")
-    public ResponseEntity<RequestResp> salaryRequestReject(@RequestBody RequestReasonDto dto){
+    public ResponseEntity<RequestResp> salaryRequestReject(@RequestBody RequestReasonDto dto) {
         RequestResp requestResp = adminService.rejectSalaryRequest(dto);
         return ResponseEntity.ok(requestResp);
     }
