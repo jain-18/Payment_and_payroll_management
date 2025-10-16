c68053cf598ad9a0838b4ed28286208dbe21197c
Sudarshan Thiruppathi <sudarshan.thiruppathi@asl.mum.sst>
2025-10-16 07:54:00 +0530

    Updated all Controllers

--- src/main/java/com/payment/controller/EmployeeController.java
+++ src/main/java/com/payment/controller/EmployeeController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -28,6 +29,7 @@ import com.payment.service.EmployeeService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 
+@CrossOrigin(origins = "http://localhost:4200/")
 @RestController
 @RequestMapping("/api/employees")
 @Validated
